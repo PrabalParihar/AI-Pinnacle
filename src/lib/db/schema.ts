1485a74d93ee54292baaf6fd02ac5ae9ef40a968
Prabal <prabalparihar95@gmail.com>
2023-06-06 14:50:33 +0530

    DrizzleORM

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,15 @@
+import { timeStamp } from "console"
+import { pgTable,serial,text ,timestamp} from "drizzle-orm/pg-core"
+
+
+
+export const $notes = pgTable('notes', {
+    id: serial('id').primaryKey(),
+    name:text('name').notNull(),
+    createdAt:timestamp('created_at').notNull().defaultNow(),
+    imageUrl : text('image_url'),
+    userId: text('user_id').notNull(),
+    editorState: text('editor_status'),
+})
+
+export type NoteType = typeof $notes.$inferInsert;
