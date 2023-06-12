849555a1dd41a38cda5959c37b09118044e7d2ea
Prabal <prabalparihar95@gmail.com>
2023-06-12 09:11:36 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import {ClerkProvider} from '@clerk/nextjs'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import Provider from "@/components/Provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'AI Pinnacle',
-
-}
+  title: "Ai Pinnacle",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
-
-       <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-
-
+      <html lang="en">
+        <Provider>
+          <body className={inter.className}>{children}</body>
+        </Provider>
+      </html>
     </ClerkProvider>
-   
-  )
+  );
 }
